e595cfa4eaf5cb139fa25f6ee458b178d2eab3c2
LB <lrbutler@gmail.com>
2021-05-05 10:12:06 -0700

    Add organization to backend

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// Web server config
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const app = express();
+
+//Middleware
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use(express.json());
+app.use(express.static("public"));
+
+//DB params
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+console.log(dbParams);
+const db = new Pool(dbParams);
+
+db.connect(() => {
+  console.log("connected to database");
+});
+
+app.get('*', (req, res) => {
+  res.send('Hi!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
