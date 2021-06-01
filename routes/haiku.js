f02a399cd93329e83d5a52d89353e7ed255f5404
LB <lrbutler@gmail.com>
2021-06-01 12:59:55 -0700

    Fix db call to work properly

--- routes/haiku.js
+++ routes/haiku.js
@@ -4,10 +4,11 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const query = `SELECT * FROM haiku`;
+    console.log("query", query)
     db.query(query, [])
-      .then((res) => {
+      .then((results) => {
         res.json(results.rows);
-        console.log(res)
+        console.log("result", res)
         res.status(201).send("Success! Enjoy the haiku!");
       })
       .catch((err) => {
